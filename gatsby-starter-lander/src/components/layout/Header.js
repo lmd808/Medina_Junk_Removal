119ef41b8873b73b4d74c8bd9e9a68b6fcaf1c7e
Laura DiTommaso <52080492+lmd808@users.noreply.github.com>
2021-05-03 09:53:25 -0400

    original files and some content editing

---
+++ gatsby-starter-lander/src/components/layout/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+import LogoIcon from '../../svg/LogoIcon';
+import Button from '../Button';
+
+const Header = () => (
+  <header className="sticky top-0 bg-white shadow">
+    <div className="container flex flex-col sm:flex-row justify-between items-center mx-auto py-4 px-8">
+      <div className="flex items-center text-2xl">
+        <div className="w-12 mr-3">
+          <LogoIcon />
+        </div>
+        Medina Removal
+      </div>
+      <div className="flex mt-4 sm:mt-0">
+      <AnchorLink className="px-4" href="#services">
+          Services
+        </AnchorLink>
+        <AnchorLink className="px-4" href="#features">
+          Commitment 
+        </AnchorLink>
+        <AnchorLink className="px-4" href="#testimonials">
+          Testimonials
+        </AnchorLink>
+      </div>
+      <div className="hidden md:block">
+        <Button className="text-sm">Get A Quote</Button>
+      </div>
+    </div>
+  </header>
+);
+
+export default Header;
